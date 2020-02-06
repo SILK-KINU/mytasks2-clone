0a37727e485d26b999b22c9b317efe2d402ba738
KINUKO SATO <gogo4641kinucivic@gmail.com>
2020-02-06 11:32:44 +0000

    20200206

--- resources/views/tasks/tweet.blade.php
+++ resources/views/tasks/tweet.blade.php
@@ -6,6 +6,7 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>オススメレシピ投稿サイト</h2>
+                <h3>投稿してください</h3>
                 <form action="{{ action('TasksController@tweet') }}" method="post" enctype="multipart/form-data">
 　　　　　　        
 　　　　　　     {{ csrf_field() }}
